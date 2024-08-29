6056fb916eeae8105a400c0f71c66d34662ac997
Daniel Lemos <daniel.o.lemos@gmail.com>
2024-08-29 09:58:38 -0300

    Second Commit

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,7 @@
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\PlayerController;
 use App\Http\Controllers\TeamController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/* Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum'); */
 
 
 Route::prefix('player')->group(function(){
